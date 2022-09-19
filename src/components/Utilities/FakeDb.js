9d8979c01061a93851a2684e880e8395ac5ab429
mdazim14 <mdazim.cse@gmail.com>
2022-09-20 01:20:01 +0600

    understand FakeDb

--- src/components/Utilities/FakeDb.js
+++ src/components/Utilities/FakeDb.js
@@ -7,18 +7,24 @@ const addToDb = id => {
     const storedCart = localStorage.getItem('shopping-cart');
     if (storedCart) {
         shoppingCart = JSON.parse(storedCart);
+        console.log("first", shoppingCart);
     }     
     else {
         shoppingCart = {};
+        console.log("shping cart empty", shoppingCart);
     }
 
     const quantity = shoppingCart[id];
+    console.log("seconded", quantity);
+
     if (quantity) {
         const newQuantity = +quantity + 1;
         shoppingCart[id] = newQuantity;
+        console.log("third", shoppingCart);
     }
     else {
         shoppingCart[id] = 1;
+        console.log(shoppingCart);
     }
     localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 
