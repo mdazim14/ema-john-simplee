afbb9e268728d980254e82c437fb5e8c3d60962e
mdazim14 <mdazim.cse@gmail.com>
2022-12-24 03:51:31 +0600

    added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -23,6 +23,8 @@ const Cart = (props) => {
                 <p>Total shipping: ${shipping}</p>
                 <p>Tax: ${tax}</p>
                 <h5>Grand Total: ${grandTotal.toFixed(2)} </h5>
+                
+                <h5>Clear Cart</h5>
         </div>
     );
 };
